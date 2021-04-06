a7c9e03fcf51769bc9ea40c074cb77656e006c80
Ahmad Zaky W <azakyw@gmail.com>
2021-04-06 14:53:28 +0700

    changes in local data source

--- Sources/Core/Data/LocalDataSource.swift
+++ Sources/Core/Data/LocalDataSource.swift
@@ -7,12 +7,12 @@
 
 import Combine
 
-public protocol LocaleDataSource {
+public protocol LocalDataSource {
     associatedtype Request
     associatedtype Response
     
     func list(request: Request?) -> AnyPublisher<[Response], Error>
     func add(entities: [Response]) -> AnyPublisher<Bool, Error>
-    func get(id: String) -> AnyPublisher<Response, Error>
-    func update(id: String, entity: Response) -> AnyPublisher<Bool, Error>
+    func get(id: Int) -> AnyPublisher<Response, Error>
+    func update(id: Int, entity: Response) -> AnyPublisher<Bool, Error>
 }
